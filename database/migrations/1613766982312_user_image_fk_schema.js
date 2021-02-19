4b5040fb2875b19d195855aa67d6c83a0225deda
Ailton Lopes <ailtonlopp@gmail.com>
2021-02-19 17:50:28 -0300

    relacionamentos user_image

---
+++ database/migrations/1613766982312_user_image_fk_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserImageFkSchema extends Schema {
+  up () {
+    this.table('users', (table) => {
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade') 
+  })
+}
+
+  down () {
+    this.table('user_image_fks', (table) => {
+      // reverse alternations
+      table.dropForeign('image_id')
+    })
+  }
+}
+
+module.exports = UserImageFkSchema
